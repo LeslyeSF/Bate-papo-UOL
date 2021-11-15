dede1e77c4cd296167d00abafdb873102cca9cbe
Leslye Soares <leslyesoares@gmail.com>
2021-11-15 15:11:14 -0300

    Selecionar destinatário/ selecionar público ou privado

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 let name_user;
-let addressee = "Todos", type = "message";
+let addressee = "Todos", type = "message", to = "Público";
 
 
 function startchat(){
@@ -11,8 +11,9 @@ function startchat(){
         const screen = document.querySelector(".inputscreen");
         screen.classList.add("minimize");
 
-        stayonline();
+        setInterval(stayonline, 5000);
         message_users();
+        setInterval(message_users, 3000);
     });
 
     promise.catch((answer) => {
@@ -24,21 +25,22 @@ function startchat(){
     
 }
 function stayonline(){
-    setInterval(() => {
-        const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: name_user});
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: name_user});
        // promise.then((answer) => {console.log(answer.data);});
-        promise.catch((erro) => {console.log(erro.data);});
-    }, 5000);
+        promise.catch((erro) => {
+            console.log(erro.data);
+            window.location.reload(true);
+        });
 }
 function message_users(){
     const messagepromisse = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
     messagepromisse.then((answer) => {
         const message = document.querySelector(".chatscreen");
         message.innerHTML="";
-        for(let i = 90; i < answer.data.length; i++){
+        for(let i = 0; i < answer.data.length; i++){
             if(answer.data[i].type === 'status'){
                 message.innerHTML +=`
-                <div class="message in-out">
+                <div class="message in-out" data-identifier="message">
                         <div class="hour">(${answer.data[i].time})</div>
                         <div class="text"><span>${answer.data[i].from}</span> ${answer.data[i].text}</div>
                 </div>
@@ -46,7 +48,7 @@ function message_users(){
             } else if(answer.data[i].type === 'private_message'){
                 if(answer.data[i].to === name_user || answer.data[i].from == name_user){
                     message.innerHTML +=`
-                    <div class="message private">
+                    <div class="message private" data-identifier="message">
                             <div class="hour">(${answer.data[i].time})</div>
                             <div class="text"><span>${answer.data[i].from}</span> reservadamente para <span>${answer.data[i].to}</span>: ${answer.data[i].text}</div>
                     </div>
@@ -54,7 +56,7 @@ function message_users(){
                 }
             } else{
                 message.innerHTML +=`
-                <div class="message">
+                <div class="message" data-identifier="message">
                         <div class="hour">(${answer.data[i].time})</div>
                         <div class="text"><span>${answer.data[i].from}</span> para <span>${answer.data[i].to}</span>: ${answer.data[i].text}</div>
                 </div>
@@ -66,10 +68,9 @@ function message_users(){
         lastmessage.scrollIntoView();
     });
     messagepromisse.catch((erro) => {
-        alert("ferrou");
-        console.log(erro.response);});
-    setInterval(message_users, 3000);
-
+        console.log(erro.response);
+        window.location.reload(true);
+    });
 }
 function send_message(){
 
@@ -77,7 +78,10 @@ function send_message(){
     if(input.classList.contains("textinput")){
         const messagepromisse = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',{from: name_user, to: addressee, text:input.value, type: type });
         //messagepromisse.then((answer) => {console.log(answer.data);});
-        messagepromisse.catch((erro) => {console.log(erro.response);});
+        messagepromisse.catch((erro) => {
+            console.log(erro.response);
+            window.location.reload(true);
+        });
         input.value = "Escreva aqui ...";
         input.classList.remove("textinput");
     }    
@@ -94,7 +98,7 @@ function users_area(){
                 participants.innerHTML += `
                 <li onclick="selected(this, 'participants')">
                     <ion-icon name="person-circle"></ion-icon>
-                    ${answer.data[i].name}
+                    <p>${answer.data[i].name}</p>
                     <ion-icon name="checkmark-outline" class="check"></ion-icon>
                 </li>
                 `;
@@ -108,6 +112,18 @@ function selected(selected, classname){
     const otherselected = document.querySelector("."+classname+" .selected");
     otherselected.classList.remove("selected");
     selected.classList.add("selected");
+
+    addressee = document.querySelector(".participants .selected p").innerHTML;
+    to = document.querySelector(".to .selected p").innerHTML;
+
+    const message_info = document.querySelector("footer p");
+    message_info.innerHTML = `Enviando para ${addressee} (${to})`;
+
+    if(to === "Público"){
+        type = "message";
+    } else{
+        type = "private_message";
+    }
     
 }
 function clickinput(input){
